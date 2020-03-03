11229939cd0d384806d916afca77e4b3add73223
Sikai Li <sli-ext@lequipe.fr>
2020-03-03 23:12:33 +0100

    resolve playback different pb

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,14 +5,18 @@ import * as Tone from "tone";
 const synth = new Tone.Synth().toMaster();
 
 const s = instance => {
-  const rects1 = JSON.parse(localStorage.getItem("shoulie"));
   const sk = instance;
-  let stop = -5;
-  const rects = [];
-  let no = 0;
-  let rect = { x: 0, y: 0, r: 30, fill: [1, 1, 1, 1], increment: 5 };
-  rects.push({ ...rect, stop });
-  const playback = true;
+  const savedRects = JSON.parse(
+    localStorage.getItem(
+      "Tue Mar 03 2020 23:07:29 GMT+0100 (Central European Standard Time)"
+    )
+  );
+  let currentRectNumber = 0;
+  let rect = { x: 0, y: 0, r: 30, fill: [1, 1, 1, 1], increment: 5, stop: -5 };
+  const rects = [{ ...rect }];
+  const playback = () => {
+    return savedRects.length > 0 && true;
+  };
   sk.setup = () => {
     sk.pixelDensity(30);
     sk.createCanvas(100, 100);
@@ -20,20 +24,21 @@ const s = instance => {
     sk.noStroke();
   };
   sk.draw = () => {
-    if (playback) {
-      rect = rects1[no];
-      stop = rect.stop;
+    if (playback()) {
+      rect = savedRects[currentRectNumber];
     }
-    if (rect.x >= Math.abs(sk.width - rect.r - stop)) {
+    if (rect.x >= Math.abs(sk.width - rect.r - rect.stop)) {
       rect.x = sk.width - rect.r;
       rect.y += rect.increment;
     } else {
       rect.x += rect.increment;
     }
-    if (rect.y > Math.abs(sk.height - rect.r - stop)) {
-      if (playback) {
-        no += 1;
+    if (rect.y > Math.abs(sk.height - rect.r - rect.stop)) {
+      if (playback()) {
+        currentRectNumber += 1;
+        rect = savedRects[currentRectNumber];
       } else {
+        // generate new rect
         rect.fill = [
           sk.random(-700, 300),
           sk.random(-255, 100),
@@ -42,22 +47,21 @@ const s = instance => {
         rect.r += (Math.random() - 0.3) * 30;
         rect.x = -5;
         rect.y = -5;
-        rects.push({ ...rect, stop });
+        rects.push({ ...rect });
       }
-      console.log(rect);
       synth.triggerAttackRelease(
         Math.abs(rect.fill[1] / rect.fill[2]) * rect.increment,
         "6"
       );
     }
-    if (rect.r > Math.abs(sk.height - stop)) {
+    if (rect.r > Math.abs(sk.height - rect.stop)) {
       rect.r = 10;
-      if (!playback) {
+      if (!playback()) {
         rect.increment = (Math.random() * 1000) / rect.r;
       }
       if (rect.increment > 3) {
-        stop += 10;
-        if (stop > sk.width) {
+        rect.stop += 10;
+        if (rect.stop > sk.width) {
           sk.noLoop();
           if (sk.pixelDensity() > 5) {
             sk.saveCanvas(
@@ -71,8 +75,7 @@ const s = instance => {
 
           setTimeout(() => {
             sk.clear();
-            rect = { x: 0, y: 0, r: 30, fill: 0, increment: 5 };
-            stop = -5;
+            rect = { x: 0, y: 0, r: 30, fill: 0, increment: 5, stop: -5 };
             sk.loop();
           }, 5000);
         }
